bdfd802425d6511e15be9b625466b937328183e8
Inez Escandon <inezescandon@Inezs-MacBook-Air.local>
2021-04-11 11:46:59 -0500

    base files

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// Get todo-text, todo-form, todo-list, todo-count from the document and store them into variables
+
+
+// Array of To-dos
+var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
