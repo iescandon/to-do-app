87e996aca6397c907d7e6c8dba294d36ee7dcdfc
Inez Escandon <inezescandon@Inezs-MacBook-Air.local>
2021-04-11 15:09:35 -0500

    add todo event listener and function

--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,29 @@ var todoInput = document.querySelector("#todo-text");
 var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
-
 // Array of To-dos
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
 function renderTodos() {
   todoList.textContent = "";
   todoCountSpan.textContent = todos.length;
-
   for (var i = 0; i < todos.length; i++) {
     var li = document.createElement("li");
     li.textContent = todos[i];
     todoList.appendChild(li)
   }
 }
+
+todoForm.addEventListener("submit", function(event){
+  event.preventDefault();
+  var newTodo = todoInput.value.trim();
+  if (newTodo === "") {
+    alert("Please type in your todo");
+    return;
+  }
+  todos.push(newTodo)
+  todoInput.value = "";
+  renderTodos();
+});
 
 renderTodos();
